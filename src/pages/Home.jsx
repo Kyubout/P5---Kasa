077c660df714e3aa7d25efd9800d60efa78d3d90
Kyubout <burdinclara@gmail.com>
2024-09-12 21:15:17 +0200

    add responsive CSS - files forgotten

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,7 +8,11 @@ function Home() {
     <main>
       <Banner
         imageUrl={bannerHome}
-        title={"Chez vous, partout et ailleurs"}
+        title={
+          <>
+            Chez vous, <span>partout et ailleurs</span>
+          </>
+        }
         opacity={"rgba(0, 0, 0, 0.6)"}
         shadow={"0px 4px 4px rgba(0, 0, 0, 0.5)"}
       />
